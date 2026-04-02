0b738c2022076ff6cce2b12ccfa5db01cb072894
HFC <j3849228@gmail.com>
2026-04-02 17:36:02 +0800

    fix: 合併 Alembic 多 head migration（cover_image + remove_categories）

---
+++ backend/alembic/versions/19ab5705135e_merge_cover_image_and_remove_categories.py
@@ -0,0 +1,26 @@
+"""merge_cover_image_and_remove_categories
+
+Revision ID: 19ab5705135e
+Revises: a19964cea032, i8j9k0l1m2n3
+Create Date: 2026-04-02 17:33:18.745335
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '19ab5705135e'
+down_revision: Union[str, None] = ('a19964cea032', 'i8j9k0l1m2n3')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
